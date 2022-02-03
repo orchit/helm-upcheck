688d1f70b6fa9da2b40a0a910be7ca1c1b2df8fc
Patrick Cornelißen <pcornelissen@orchit.de>
2022-02-03 13:19:57 +0100

    fix log_level default value syntax

--- files/update.sh
+++ files/update.sh
@@ -4,7 +4,7 @@ cd /usr/src/app/web ||exit
 
 . ../env
 echo "Gathering metrics..."
-nova find --wide --output-file versions -v ${LOG_LEVEL:1} ${NOVA_PARAMETERS} 2> lastlog
+nova find --wide --output-file versions -v ${LOG_LEVEL} ${NOVA_PARAMETERS} 2> lastlog
 transform.sh versions > metrics
 chmod 666 versions metrics lastlog || true
 echo "Done"
